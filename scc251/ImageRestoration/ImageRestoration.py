1e4a27434f9b2b4bab1f741fe867e299d78276e5
Ana Costa <ana_costa@usp.br>
2020-06-26 17:56:08 -0300

    PDI: Short Assignment 2 added

--- scc251/ImageRestoration/ImageRestoration.py
+++ scc251/ImageRestoration/ImageRestoration.py
@@ -8,30 +8,73 @@ def gaussian_filter(k=3, sigma=1.0):
     filt = np.exp( -(1/2)*(np.square(x) + np.square(y))/np.square(sigma) )
     return filt/np.sum(filt)
 
-#def image_normalization(f):
-#   return (255 * (f - f.min())/(f.max() - f.min())
+def normalize(f, max):
+	return (max * (f - f.min()))/(f.max() - f.min())
 
-# parameters input
-filename = str(input()).strip()
-k = int(input())
-sigma = float(input())
-gamma = float(input())
+def denoising(g, w):
+	"""Function that denoises image
+	Params:
+		g - image input
+		w - guassian filter
+	"""
+	# padding filter
+	pad = int(g.shape[0]//2 - w.shape[0]//2) 
+	wp = np.pad(w, (pad,pad-1), 'constant', constant_values=(0))
 
-img = imageio.imread(filename)
-h = gaussian_filter(k, sigma)
+	# applying the fourier transform to image and filter
+	G = fftn(g)
+ 	W = fftn(wp)
 
-# padding image to multiply  
-a = int(img.shape[0]//2 - h.shape[0]//2) 
-h_pad = np.pad(h, (a,a-1), 'constant', constant_values=(0))
+	denoised_img = np.multiply(G, W)
+    denoised_img = np.real(fftshift(ifftn(denoised_img)))
 
-G = fftn(img)
-H = fftn(h_pad)
+	return denoised_img
 
-# image denoising 
-denoised_img = np.multiply(G, H)
+def deblurring(g, h, gamma):
+	"""Function that restores the blur using the Constrained Least Squares method
+	Params:
+		g - degraded image
+		h - degradation function (gaussian filter)
+		gamma - regularization factor
+	"""
 
-# normalizing image
-denoised_img = 255 * (denoised_img - denoised_img.min()) / (denoised_img.max() - denoised_img.min())
+	# defining laplace operator
+	p = np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]])
 
-output_img = ifftn(denoised_img)
-imageio.imwrite("denoiused_img_b1_n.png", output_img)
+	pad = int(g.shape[0]//2 - h.shape[0]//2) 
+    hp = np.pad(h, (pad,pad-1), 'constant', constant_values=(0))
+    pad = int(g.shape[0]//2 - p.shape[0]//2) 
+    pp = np.pad(p, (pad,pad-1), 'constant', constant_values=(0))
+
+	# applying the fourier transform
+	G = fftn(g)
+    H = fftn(hp)
+    P = fftn(pp)
+
+    F = (np.conjugate(H) / (np.square(np.abs(H)) + gamma*np.square(np.abs(P)))) * G
+    f = np.real(fftshift(ifftn(F)))
+
+    return f
+
+def main():
+	# parameters input
+	filename = str(input()).strip()
+	k = int(input())
+	sigma = float(input())
+	gamma = float(input())
+
+	img = imageio.imread(filename)
+	w = gaussian_filter(k, sigma)
+	maxg = img.max()
+
+	denoised_img = denoising(img, w)
+	maxd = denoised_img.max()
+	denoised_img = normalization(denoised_img, maxg)
+
+	restored_img = deblurring(denoised_img, w, gamma)
+	restored_img = normalization(restored_img, maxd)
+
+	print(np.round(np.std(restored_img),1))
+
+if __name__ == '__main__':
+	main()
